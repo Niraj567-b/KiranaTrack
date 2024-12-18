448de1efd5e07d6e8d7f0d3129ff12b00544b33a
Niraj567-b <nirajpal1209@gmail.com>
2024-12-18 19:03:36 +0000

    added customer page

--- store/models.py
+++ store/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
+GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ]
+
 class Customer(models.Model):
 
     name = models.CharField(max_length = 100, blank = False, null = False)
     phone = models.CharField(max_length = 10, blank = False, null = False)
-    AadharNo = models.CharField(max_length = 10, blank = False, null = False, unique = True)
+    AadharNo = models.CharField(max_length = 12, blank = False, null = False, unique = True)
+    gender = models.CharField(max_length=1,choices=GENDER_CHOICES,default='M')
     address = models.TextField(blank = False, null = False)
 
     def __str__(self):
@@ -15,6 +22,7 @@ class Staff(models.Model):
     staffName = models.CharField(max_length = 100, blank = False, null = False)
     phone = models.CharField(max_length = 10, blank = False, null = False)
     address = models.TextField(blank = False, null = False)
+    gender = models.CharField(max_length=1,choices=GENDER_CHOICES,default='M')
 
     def __str__(self):
         return self.staffName
--- store/views.py
+++ store/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from .models import Customer
 
 def home (request):
     return render(request,'home.html')
 
 
 def customer (request):
-    return render(request,'customer.html')
+
+    customers = Customer.objects.all()
+
+    return render(request,'customer.html',{'customers': customers})
 
 
 def staff (request):
